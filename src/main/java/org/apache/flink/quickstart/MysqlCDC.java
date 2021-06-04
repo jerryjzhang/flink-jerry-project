f40ad11893f62aedc7f6538a8ee6dca71e60cd95
jerryjzhang <zhangjun2915@163.com>
2021-06-04 10:58:51 +0800

    MysqlCDC: Add jdbc sink

--- src/main/java/org/apache/flink/quickstart/MysqlCDC.java
+++ src/main/java/org/apache/flink/quickstart/MysqlCDC.java
@@ -5,7 +5,7 @@ import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class MysqlCDC {
-    public static void main(String [] args) throws Exception {
+    public static void main(String [] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env,
                 EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build());
@@ -28,32 +28,51 @@ public class MysqlCDC {
                 "localhost", 3306, "jerryjzhang", "tme", "jerry", "products");
 
         String sinkDDL = "CREATE TABLE product_sink (" +
+                " id INT," +
                 " name STRING," +
                 " weight DECIMAL(10,3)," +
-                " PRIMARY KEY (name) NOT ENFORCED" +
+                " PRIMARY KEY (id) NOT ENFORCED" +
                 ") WITH (" +
                 " 'connector' = 'print'" +
                 ")";
 
         String sinkEsDDL = "CREATE TABLE product_es (\n" +
+                " id INT," +
                 "name STRING,\n" +
                 "weight DECIMAL(10,3),\n" +
-                "PRIMARY KEY(name) NOT ENFORCED\n" +
+                "PRIMARY KEY(id) NOT ENFORCED\n" +
                 ") WITH (\n" +
                 "  'connector' = 'elasticsearch-7',\n" +
                 "  'hosts' = 'http://localhost:9200',\n" +
                 "  'index' = 'jerry_product'\n" +
                 ")";
 
+        String sinkMysqlDDL = String.format(
+                "CREATE TABLE product_sink_db (" +
+                        " id INT NOT NULL," +
+                        " name STRING," +
+                        " description STRING," +
+                        " weight DECIMAL(10,3)," +
+                        " PRIMARY KEY(id) NOT ENFORCED\n" +
+                        ") WITH (" +
+                        " 'connector' = 'jdbc'," +
+                        " 'url' = '%s'," +
+                        " 'username' = '%s'," +
+                        " 'password' = '%s'," +
+                        " 'table-name' = '%s'" +
+                        ")",
+                "jdbc:mysql://localhost:3306/jerry", "jerryjzhang", "tme", "products_sink");
+
         tEnv.executeSql(sourceDDL);
         tEnv.executeSql(sinkDDL);
         tEnv.executeSql(sinkEsDDL);
+        tEnv.executeSql(sinkMysqlDDL);
 
         tEnv.executeSql("INSERT INTO product_es SELECT name, weight FROM " +
                 "product_source");
-        tEnv.executeSql("INSERT INTO product_sink SELECT name, weight FROM " +
+        tEnv.executeSql("INSERT INTO product_sink SELECT id, name, weight FROM " +
                 "product_source");
-
-        env.execute("CDC demo");
+        tEnv.executeSql("INSERT INTO product_sink_db SELECT * FROM " +
+                "product_source");
     }
 }
